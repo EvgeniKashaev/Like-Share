3da3b28e572f695de9fe735fdd54f174d0567bf0
EvgeniKashaev <127733834+EvgeniKashaev@users.noreply.github.com>
2024-02-08 16:05:50 +0300

    mvvm 1.3

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -6,7 +6,7 @@ data class Post(
     val content: String,
     val published: String,
     val likedByMe: Boolean = true,
-    val likes: Int = 5,
+    val likes: Int = 4,
     val shares: Int = 5,
 )
 
